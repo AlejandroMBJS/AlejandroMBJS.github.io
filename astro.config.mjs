4301098375162dcc1373dcc28a6fa2bea4c569dd
AlejandroMBJS <a.mejiabjs@gmail.com>
2024-09-27 13:46:05 -0600

    deploycommitvercel6

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
+  integrations: [tailwind(), vercel()],
   output: 'server',
   adapter: vercel(),
 });
