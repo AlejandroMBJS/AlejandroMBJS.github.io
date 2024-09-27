0b1280757675e0246f4ec82f2821acf26793eb9d
AlejandroMBJS <a.mejiabjs@gmail.com>
2024-09-27 10:26:57 -0600

    deploycommit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,8 @@ import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
-import vercel from '@astrojs/vercel/serverless';
-
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-});)
+  integrations: [tailwind(), robotsTxt()],
+  site: 'https://AlejandroMBJS.github.io',
+  base: 'Portfolio'
+})
