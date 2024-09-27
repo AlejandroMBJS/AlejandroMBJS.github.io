f954f7bcf837f928f7f03834ec428e802e1c29fe
AlejandroMBJS <a.mejiabjs@gmail.com>
2024-09-27 14:05:23 -0600

    deploycommitaws2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
 
+import robotsTxt from "astro-robots-txt"
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [vercel()],
-  output: 'server',
-  adapter: vercel(),
-});
+  integrations: [tailwind(), robotsTxt()],
+})
